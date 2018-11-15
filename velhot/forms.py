3e967418dde8a127f77e0f8c8b1e13644c36fbf4
Henrik Sillanpää <sillan44@stundet.tut.fi>
2018-11-15 13:46:47 +0200

    Changed password working

--- velhot/forms.py
+++ velhot/forms.py
@@ -23,4 +23,3 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField()
-
--- velhot/views.py
+++ velhot/views.py
@@ -6,6 +6,12 @@ from velhot.forms import SignUpForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 def profile(user):
     return HttpResponse("Ripuli.")
@@ -42,5 +48,19 @@ def logout_view(request):
     logout(request)
     return redirect('/')
 
-def settings(user):
-    return HttpResponse("You can change your profile settings here.")
+@login_required
+def settings(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('settings')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
